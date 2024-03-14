84e354cdd4ccdaf6c09c09e129a737edf73a4c67
chantiboschetti <97052239+chantiboschetti@users.noreply.github.com>
2024-03-14 16:59:20 -0400

    lab_8.c

--- lab_8.c
+++ lab_8.c
@@ -27,54 +27,73 @@ size_t Size(void* ptr)
 	return ((size_t*)ptr)[-1];
 }
 
-void merge(int arr[], int l, int m, int r) {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
+void merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
 
-    int L[n1], R[n2];
+/* create temp arrays */
+int *L = (int*) Alloc(n1*sizeof(int));
+int *R = (int*) Alloc(n2*sizeof(int));
 
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
-
-    i = 0;
-    j = 0; 
-    k = l;
-    
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1+ j];
+/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = l; // Initial index of merged subarray
+while (i < n1 && j < n2)
+{
+	if (L[i] <= R[j])
+	{
+		arr[k] = L[i];
+		i++;
+	}
+	else
+	{
+		arr[k] = R[j];
+		j++;
+	}
+	k++;
+}
+/* Copy the remaining elements of L[], if there
+are any */
+while (i < n1)
+{
+	arr[k] = L[i];
+	i++;
+	k++;
+}
+/* Copy the remaining elements of R[], if there
+are any */
+while (j < n2)
+{
+	arr[k] = R[j];
+	j++;
+	k++;
+}
+	DeAlloc(L);
+	DeAlloc(R);
+}
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+	if (l < r)
+	{
+// get the mid point
+		int m = (l+r)/2;
+// Sort first and second halves
+	mergeSort(arr, l, m);
+	mergeSort(arr, m+1, r);
+// printf("Testing l=%d r=%d m=%d\n", l, r, m);
+	merge(arr, l, m, r);
 }
 
-void mergeSort(int arr[], int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);
-    }
 }
 
 // parses input file to an integer array
